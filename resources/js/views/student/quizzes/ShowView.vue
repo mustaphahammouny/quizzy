98d8e986e1ca97bc8cf93ae2dd017fd45e3282b6
Mustapha <mhammouny@rc2k.fr>
2023-11-27 18:50:13 +0100

    test

--- resources/js/views/student/quizzes/ShowView.vue
+++ resources/js/views/student/quizzes/ShowView.vue
@@ -103,18 +103,6 @@ onBeforeMount(async () => {
                             <div class="fs-sm text-muted">Email: {{ quiz.teacher.email }}</div>
                             <div class="fs-sm text-muted">Since: {{ quiz.teacher.created_at }}</div>
                         </div>
-
-                        <div class="dropdown">
-                            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown"
-                                aria-expanded="false">
-                                Dropdown button
-                            </button>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </div>
                     </BaseBlock>
                 </div>
             </div>
